b1b95ecb86df6aa12c2d347eb44089a636ddddaa
ericms2016 <eric.ms2016@gmail.com>
2016-10-18 21:45:41 -0700

    Update buttonCounter.js

--- buttonCounter.js
+++ buttonCounter.js
@@ -1,1 +1,17 @@
 # estimate-situation
+<script type="text/javascript">
+    var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+	if (clicks === 1){
+	return “Congratulations!”
+	}
+	if (clicks === 3){
+	return “20% discount on all products!”
+	}
+	if (clicks === 5)
+	return “Hurray!!! 50% off on all items!”
+	}
+    </script>
